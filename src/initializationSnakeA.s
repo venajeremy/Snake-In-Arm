afc80f9d403229d9e07eb9b645c187bde349e1e6
Pedro <plhrgt@gmail.com>
2024-12-14 14:39:07 -0800

    update

--- src/initializationSnakeA.s
+++ src/initializationSnakeA.s
@@ -1,101 +1,130 @@
-.section .data
-width:      .word 10      // Example value for width
-height:     .word 10      // Example value for height
-
 .section .bss
-snakeSize:  .space 4      // Reserve space for an integer
-head:       .space 8      // Reserve space for a pointer
-body:       .space 8      // Reserve space for a pointer
-tail:       .space 8      // Reserve space for a pointer
+inHead:    .space 8    // Reserve space for snakePart* inHead
+inTail:    .space 8    // Reserve space for snakePart* inTail
+tmp:       .space 8    // Reserve space for snakePart* tmp
+
+.section .data
+width:     .word 10    // Example width
+height:    .word 10    // Example height
 
 .section .text
-.global _start
+.global initializeSnake
 
-_start:
-    ldr x0, =snakeSize
-    mov w1, #0
-    str w1, [x0]
+initializeSnake:
+    // Prologue
+    stp x29, x30, [sp, #-16]!
+    mov x29, sp
 
-    // Allocate memory for head, body, and tail
-    mov x0, #16           // size of snakePart
-    bl malloc
-    mov x20, x0           // head
+    // Arguments: 
+    // x0 -> inHead
+    // x1 -> inTail
+    // w2 -> snakeChar
+    // w3 -> snakeSize
+    // w4 -> width
+    // w5 -> height
+    // x6 -> key
+    // x7 -> hand
+    // x8 -> map
 
-    mov x0, #16
-    bl malloc
-    mov x21, x0           // body
+    // Call createSnakePart() for head
+    bl createSnakePart
+    str x0, [x0]             // *inHead = createSnakePart()
 
-    mov x0, #16
-    bl malloc
-    mov x22, x0           // tail
+    // Set head x and y positions
+    ldr w9, =width
+    lsr w9, w9, #1           // width / 2
+    str w9, [x0]             // (*inHead)->xpos
 
-    // Initialize head
-    ldr w0, =width
-    ldr w0, [w0]
-    lsr w0, w0, #1        // width / 2
-    str w0, [x20]         // head->xpos
+    ldr w10, =height
+    lsr w10, w10, #1         // height / 2
+    str w10, [x0, #4]        // (*inHead)->ypos
 
-    ldr w0, =height
-    ldr w0, [w0]
-    lsr w0, w0, #1        // height / 2
-    str w0, [x20, #4]     // head->ypos
+    // Initialize tmp = *inHead
+    str x0, [tmp]
 
-    str x21, [x20, #8]    // head->backward
+    // Loop to initialize body parts
+    mov w11, #1              // i = 1
+loop_body:
+    cmp w11, w3              // compare i with snakeSize
+    b.ge end_body_loop       // if i >= snakeSize, exit loop
 
-    // Initialize body
-    ldr w0, =width
-    ldr w0, [w0]
-    lsr w0, w0, #1
-    sub w0, w0, #1        // (width / 2) - 1
-    str w0, [x21]         // body->xpos
+    // Create new snakePart for body
+    bl createSnakePart
 
-    ldr w0, =height
-    ldr w0, [w0]
-    lsr w0, w0, #1
-    str w0, [x21, #4]     // body->ypos
+    // Set backward link
+    ldr x12, [tmp]
+    str x12, [x0, #12]       // tmp->backward = new snakePart
 
-    str x20, [x21, #8]    // body->forward
-    str x22, [x21, #12]   // body->backward
+    // Set forward link
+    str x0, [x12, #8]        // tmp->backward->forward = tmp
 
+    // Move tmp to the new part
+    str x0, [tmp]
+
+    // Set x and y positions for the new part
+    ldr w13, =width
+    lsr w13, w13, #1
+    add w13, w13, w11        // (width/2) + i
+    str w13, [x0]
+
+    ldr w14, =height
+    lsr w14, w14, #1         // height / 2
+    str w14, [x0, #4]
+
+    // Increment i
+    add w11, w11, #1
+    b loop_body              // repeat loop
+
+end_body_loop:
     // Initialize tail
-    ldr w0, =width
-    ldr w0, [w0]
-    lsr w0, w0, #1
-    sub w0, w0, #2        // (width / 2) - 2
-    str w0, [x22]         // tail->xpos
+    bl createSnakePart
+    ldr x15, [tmp]
+    str x15, [x0, #8]        // (*inTail)->forward = tmp
+    str x0, [x15, #12]       // tmp->backward = *inTail
 
-    ldr w0, =height
-    ldr w0, [w0]
-    lsr w0, w0, #1
-    str w0, [x22, #4]     // tail->ypos
+    // Set tail x and y positions
+    ldr w16, =width
+    lsr w16, w16, #1
+    add w16, w16, w3
+    sub w16, w16, #1         // (width/2) + snakeSize - 1
+    str w16, [x0]            // (*inTail)->xpos
 
-    str x21, [x22, #8]    // tail->forward
+    ldr w17, =height
+    lsr w17, w17, #1         // height / 2
+    str w17, [x0, #4]        // (*inTail)->ypos
 
     // Paint snake onto map
-    mov x23, x20          // tempPart = head
+    ldr x18, [tmp]
+    mov w19, #1              // int32_t i = 1
 
-loop:
-    cmp x23, #0
-    b.eq endloop          // while(tempPart != NULL)
+paint_loop:
+    cmp x18, #0
+    b.eq end_paint_loop      // while(tmp != NULL)
 
-    // Increment snakeSize
-    ldr x0, =snakeSize
-    ldr w1, [x0]
-    add w1, w1, #1
-    str w1, [x0]
+    // Placeholder for swapKeyValuesA (Not implemented)
+    // bl swapKeyValuesA
 
-    // Assuming swapKeyValues and map modification (pseudo code)
-    // bl swapKeyValues
-    // modify map[tempPart->ypos][tempPart->xpos]
+    // Paint snake character onto the map
+    ldr w20, [x18, #4]       // tmp->ypos
+    mul w21, w20, w4         // (tmp->ypos * width)
+    ldr w22, [x18]           // tmp->xpos
+    add w21, w21, w22        // (tmp->ypos * width) + tmp->xpos
+    ldr x23, [x8, x21, LSL #3] // map[tmp->ypos]
+    strb w2, [x23, w22]      // map[tmp->ypos][tmp->xpos] = snakeChar
 
-    ldr x23, [x23, #12]   // tempPart = tempPart->backward
-    b loop
+    // Move to the next part
+    ldr x18, [x18, #12]      // tmp = tmp->backward
 
-endloop:
-    // Exit program
-    mov x0, #0
-    mov x8, #93
-    svc #0
+    // Increment i
+    add w19, w19, #1
+    b paint_loop
 
-// trying to see of this works 
-// 
+end_paint_loop:
+    // Epilogue
+    ldp x29, x30, [sp], #16
+    ret
+
+createSnakePart:
+    // Placeholder for function
+    // Returns pointer to new snakePart
+    ret
